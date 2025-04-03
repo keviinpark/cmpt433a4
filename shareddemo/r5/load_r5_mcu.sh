a0647f12ee455c7458654af6a728c2f61c6244e2
Jesse <jessebot.git@gmail.com>
2025-04-03 14:41:28 -0700

    merged sharedMem and neopixel

--- shareddemo/r5/load_r5_mcu.sh
+++ shareddemo/r5/load_r5_mcu.sh
@@ -13,5 +13,5 @@ echo "Starting: /sys/class/remoteproc/remoteproc2/state"
 echo start | sudo tee /sys/class/remoteproc/remoteproc2/state
 
 echo "WORK AROUND: Reading/Writing the pins to set the MUX / direction"
-gpioset gpiochip0 9=1
-gpioget gpiochip0 10
+gpioset gpiochip0 7=1   # LED Stick
+gpioget gpiochip0 10    # Rotary Encoder - Push
--- shareddemo/r5/src/main.c
+++ shareddemo/r5/src/main.c
@@ -22,12 +22,42 @@ static void *pSharedMem = (void *) SHARED_MEM_BTCM_START;
 #define MICRO_SECONDS_PER_MILI_SECOND   (1000)
 #define DEFAULT_LED_DELAY_MS            (100)
 
+
+// NeoPixel Timing
+// NEO_<one/zero>_<on/off>_NS
+// (These times are what the hardware needs; the delays below are hand-tuned to give these).
+#define NEO_NUM_LEDS          8   // # LEDs in our string
+//
+#define NEO_ONE_ON_NS       700   // Stay on 700ns
+#define NEO_ONE_OFF_NS      600   // (was 800)
+#define NEO_ZERO_ON_NS      350
+#define NEO_ZERO_OFF_NS     800   // (Was 600)
+#define NEO_RESET_NS      60000   // Must be at least 50us, use 60us
+
+// Delay time includes 1 GPIO set action.
+volatile int junk_delay = 0;
+#define DELAY_350_NS() {}
+#define DELAY_600_NS() {for (junk_delay=0; junk_delay<9 ;junk_delay++);}
+#define DELAY_700_NS() {for (junk_delay=0; junk_delay<16 ;junk_delay++);}
+#define DELAY_800_NS() {for (junk_delay=0; junk_delay<23 ;junk_delay++);}
+
+#define DELAY_NS(ns) do {int target = k_cycle_get_32() + k_ns_to_cyc_near32(ns); \
+	while(k_cycle_get_32() < target) ; } while(false)
+
+#define NEO_DELAY_ONE_ON()     DELAY_700_NS()
+#define NEO_DELAY_ONE_OFF()    DELAY_600_NS()
+#define NEO_DELAY_ZERO_ON()    DELAY_350_NS()
+#define NEO_DELAY_ZERO_OFF()   DELAY_800_NS()
+#define NEO_DELAY_RESET()      {DELAY_NS(NEO_RESET_NS);}
+
 // Device tree nodes for pin aliases
 #define LED0_NODE DT_ALIAS(led0)
 #define BTN0_NODE DT_ALIAS(btn0)
+#define NEOPIXEL_NODE DT_ALIAS(neopixel)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec btn = GPIO_DT_SPEC_GET(BTN0_NODE, gpios);
+static const struct gpio_dt_spec neopixel = GPIO_DT_SPEC_GET(NEOPIXEL_NODE, gpios);
 
 static void initialize_gpio(const struct gpio_dt_spec *pPin, int direction) 
 {
@@ -43,29 +73,39 @@ static void initialize_gpio(const struct gpio_dt_spec *pPin, int direction)
 	}
 }
 
-/* 
-	Problem with Structs:
-	- When access data via a pointer-to-a-struct, program hangs.
-
-	Investigated:
-	- Changing field types in struct to 32-bit does not help.
-	- Adding alignmnet attribute to struct did not help.
-	- Unable to read or write via struct pointer
-	- Not an issue of buffering stdout
-	- When changed char array to 32-bit vaules form 8, in hangs on printing?
-
-	Works:
-	- Access via a pointer, instead of struct pointer.
-	- Access via an array
-*/
-
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
 
 	initialize_gpio(&led, GPIO_OUTPUT_ACTIVE);
 	initialize_gpio(&btn, GPIO_INPUT);
+	initialize_gpio(&neopixel, GPIO_OUTPUT_ACTIVE);
 
+
+	// COLOURS
+	// - 1st element in array is 1st (bottom) on LED strip; last element is last on strip (top)
+	// - Bits: {Green/8 bits} {Red/8 bits} {Blue/8 bits} {White/8 bits}
+	uint32_t color[NEO_NUM_LEDS] = {
+		0x0f000000, // Green
+		0x000f0000, // Red
+		0x00000f00, // Blue
+		0x0000000f, // White
+		0x0f0f0f00, // White (via RGB)
+		0x0f0f0000, // Yellow
+		0x000f0f00, // Purple
+		0x0f000f00, // Teal
+
+		// Try these; they are birght! 
+		// (You'll need to comment out some of the above)
+		// 0xff000000, // Green Bright
+		// 0x00ff0000, // Red Bright
+		// 0x0000ff00, // Blue Bright
+		// 0xffffff00, // White
+		// 0xff0000ff, // Green Bright w/ Bright White
+		// 0x00ff00ff, // Red Bright w/ Bright White
+		// 0x0000ffff, // Blue Bright w/ Bright White
+		// 0xffffffff, // White w/ Bright White
+	};
 
 	printf("Contents of Shared Memory ATCM:\n");
 	for (int i = 0; i < END_MEMORY_OFFSET; i++) {
@@ -96,24 +136,52 @@ int main(void)
 	MEM_UINT32(IS_BUTTON_PRESSED_OFFSET) = 0;
 	MEM_UINT32(BTN_COUNT_OFFSET) = 0;
 	MEM_UINT32(LOOP_COUNT_OFFSET) = 0;
-	
+
 	printf("Contents of Shared Memory ATCM After Write:\n");
 	for (int i = 0; i < END_MEMORY_OFFSET; i++) {
 		char* addr = (char*)pSharedMem + i;
 		printf("0x%08x = %2x (%c)\n", (uint32_t) addr, *addr, *addr);
 	}
 
-
 	bool led_state = true;
 	uint32_t btnCount = 0;
 	uint32_t loopCount = 0;
 	while (true) {
-		// Toggle LED
-		printf("LED state: %s\n", led_state ? "OFF" : "ON");
-		if (gpio_pin_toggle_dt(&led) < 0) {
-			printf("ERROR: GPIO Pin Toggle DT\n");
-			return 0;
+		// testing switching colors
+		if (loopCount > 300) {
+			color[7] = 0xff000000;
+			color[6] = 0xff000000;
+			color[5] = 0xff000000;
+			color[4] = 0xff000000;
+			color[3] = 0xff000000;
+			color[2] = 0xff000000;
+			color[1] = 0xff000000;
+			color[0] = 0xff000000;
 		}
+
+		// Neopixel (led state basically unused)
+		printf("LED state: %s\n", led_state ? "OFF" : "ON");
+		gpio_pin_set_dt(&neopixel, 0);
+		DELAY_NS(NEO_RESET_NS);
+
+		for(int j = 0; j < NEO_NUM_LEDS; j++) {
+			for(int i = 31; i >= 0; i--) {
+				if(color[j] & ((uint32_t)0x1 << i)) {
+					gpio_pin_set_dt(&neopixel, 1);
+					NEO_DELAY_ONE_ON();
+					gpio_pin_set_dt(&neopixel, 0);
+					NEO_DELAY_ONE_OFF();
+				} else {
+					gpio_pin_set_dt(&neopixel, 1);
+					NEO_DELAY_ZERO_ON();
+					gpio_pin_set_dt(&neopixel, 0);
+					NEO_DELAY_ZERO_OFF();
+				}
+			}
+		}
+
+		gpio_pin_set_dt(&neopixel, 0);
+		NEO_DELAY_RESET();
 		led_state = !led_state;
 
 
